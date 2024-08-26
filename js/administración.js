7b24e59f3a80851a1cff4dad2f53ce678df1f180
Favio Terrones <fterronesc@alicorp.com.pe>
2024-08-26 00:22:02 -0500

    add creacionUsuario js

--- js/administración.js
+++ js/administración.js
@@ -1,3 +1,72 @@
+
+
+const crearUsuarioApi = "https://www.pruebaconex.somee.com/api/usuario"
+const btnAbrirlModalCrearUsuario = document.getElementById("btnCrearUsuario");
+const btnCerrarModalUsuario = document.getElementById("btnCerrarModalCrearUsuario");
+
+
+
+
+btnCerrarModalUsuario.onclick = () => document.getElementById("crearUsuarioModal").classList.add("hidden");
+btnAbrirlModalCrearUsuario.onclick = () => document.getElementById("crearUsuarioModal").classList.remove("hidden");
+
+
+
+document.getElementById("crearUsuarioFormulario").addEventListener("submit",(e)=>{
+          e.preventDefault();
+          
+          const usuario = document.getElementById("crearUsuarioFormulario")["nombreUsuario"].value;
+          const contra = document.getElementById("crearUsuarioFormulario")["contraUsuario"].value;
+          const crearCotizacion = document.querySelector('input[name="crearCotizacion"]').checked;
+          const editarCotizacion = document.querySelector('input[name="editarCotizacion"]').checked;
+          const crearProducto = document.querySelector('input[name="crearProducto"]').checked;
+          const editarProducto = document.querySelector('input[name="editarProducto"]').checked;
+          const estadoUsuario = document.getElementById("estadoUsuario").checked;
+
+          const nuevoUsuario = {
+            Usuario:usuario,
+            Psswd:contra,
+            Auto_Crear_Cot: crearCotizacion,
+            Auto_Editar_Cot:editarCotizacion,
+            Auto_Crear_Prod: crearProducto,
+            Auto_Editar_Prod: editarProducto,
+            Activo: estadoUsuario
+        };
+
+ 
+          
+        crearUsuario(nuevoUsuario).then(() => {
+            Swal.fire({
+                title: 'Usuario Creado',
+                text: 'El usuario ha sido creado exitosamente.',
+                icon: 'success',
+                confirmButtonText: 'OK'
+            }).then(() => {
+                location.reload();
+            });
+        });
+
+          
+});
+
+ // Función para crear un nuevo usuario
+async function crearUsuario(usuario) {
+    try {
+        const response = await fetch(crearUsuarioApi, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuario)
+        });
+        if (!response.ok) throw new Error('Error al crear el usuario');
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://www.pruebaconex.somee.com/api/usuario/listar')
         .then(response => response.json())
