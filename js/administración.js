fbfed57b433d42da76a0078cc51734cb81aad3c5
Favio Terrones <fterronesc@alicorp.com.pe>
2024-08-26 23:15:26 -0500

    add editar usuarios

--- js/administración.js
+++ js/administración.js
@@ -1,6 +1,7 @@
 
 
 const crearUsuarioApi = "https://www.pruebaconex.somee.com/api/usuario"
+const actualizarUsuarioApi = "https://www.pruebaconex.somee.com/api/usuario/"
 const btnAbrirlModalCrearUsuario = document.getElementById("btnCrearUsuario");
 const btnCerrarModalUsuario = document.getElementById("btnCerrarModalCrearUsuario");
 
@@ -66,6 +67,131 @@ async function crearUsuario(usuario) {
     }
 }
 
+async function actualizarEstado(id, usuario) {
+    let estadoInicial = usuario.Activo;
+    let { Psswd, Auto_Crear, Auto_Crear_Prod, Auto_Editar, Auto_Editar_Prod } = usuario;
+
+    
+    let nuevoEstadoUsuario = {
+        Psswd,
+        Auto_Crear_Cot: Auto_Crear,
+        Auto_Editar_Cot: Auto_Editar,
+        Auto_Crear_Prod,
+        Auto_Editar_Prod,
+        Activo: !estadoInicial
+    };
+
+    try {
+        const response = await fetch(`${actualizarUsuarioApi}${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoEstadoUsuario)
+        });
+
+        if (!response.ok) throw new Error('Error al cambiar estado');
+
+        Swal.fire({
+            title: 'Estado cambiado',
+            text: 'El estado del usuario se cambió exitosamente.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        }).then(() => {
+            location.reload(); // Se recarga la página cuando el usuario hace clic en 'OK'
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+        Swal.fire({
+            title: 'Error',
+            text: 'Hubo un problema al cambiar el estado del usuario.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+}
+
+
+document.getElementById('closeEditUsuarioModal').onclick = () => document.getElementById('editUsuarioModal').classList.add('hidden');
+
+
+async function mostrarModalEditarUsuario(usuario) {
+
+    document.getElementById("edicionNombreUsuario").value = usuario.Usuario;
+    document.getElementById('edicionContraUsuario').value = usuario.Psswd;
+
+    // Selecciona el radio button correspondiente para 'edicionCrearCotizacion'
+    document.querySelector(`input[name="edicionCrearCotizacion"][value="${usuario.Auto_Crear}"]`).checked = true;
+    // Selecciona el radio button correspondiente para 'edicionEditarCotizacion'
+    document.querySelector(`input[name="edicionEditarCotizacion"][value="${usuario.Auto_Editar}"]`).checked = true;
+    // Selecciona el radio button correspondiente para 'edicionCrearProducto'
+    document.querySelector(`input[name="edicionCrearProducto"][value="${usuario.Auto_Crear_Prod}"]`).checked = true;
+    // Selecciona el radio button correspondiente para 'edicionEditarProducto'
+    document.querySelector(`input[name="edicionEditarProducto"][value="${usuario.Auto_Editar_Prod}"]`).checked = true;
+    // Marca el checkbox del estado del usuario
+    document.getElementById("edicionEstadoUsuario").checked = usuario.Activo;
+    // Muestra el modal
+    document.getElementById('editUsuarioModal').classList.remove('hidden');
+}
+
+
+const editUsuarioForm = document.getElementById('editUsuarioForm');
+editUsuarioForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    let id = document.getElementById("edicionNombreUsuario").value;
+
+    const contra = document.getElementById('edicionContraUsuario').value;
+    const crearCotizacion = document.querySelector('input[name="edicionCrearCotizacion"]').checked;
+    const editarCotizacion = document.querySelector('input[name="edicionEditarCotizacion"]').checked;
+    const crearProducto = document.querySelector('input[name="edicionCrearProducto"]').checked;
+    const editarProducto = document.querySelector('input[name="edicionEditarProducto"]').checked;
+    const estadoUsuario = document.getElementById("edicionEstadoUsuario").checked;
+
+    let usuarioActualizado = {
+        Psswd:contra,
+        Auto_Crear_Cot: crearCotizacion,
+        Auto_Editar_Cot: editarCotizacion,
+        Auto_Crear_Prod:crearProducto,
+        Auto_Editar_Prod:editarProducto,
+        Activo: estadoUsuario
+    };
+
+
+
+    try {
+        const response = await fetch(`${actualizarUsuarioApi}${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(usuarioActualizado)
+        });
+
+        if (!response.ok) throw new Error('Error al actualizar usuario');
+
+        Swal.fire({
+            title: 'Usuario actualizado',
+            text: 'El usuario se actualizo exitosamente.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+        }).then(() => {
+            location.reload(); // Se recarga la página cuando el usuario hace clic en 'OK'
+        });
+
+    } catch (error) {
+        console.error('Error:', error);
+        Swal.fire({
+            title: 'Error',
+            text: 'Hubo un problema al intentar actualizar el usuario.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+        });
+    }
+
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
     fetch('https://www.pruebaconex.somee.com/api/usuario/listar')
@@ -102,16 +228,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 accionesCell.classList.add('py-3', 'px-6','border','border-gray-300','text-center');
 
                 const editarButton = document.createElement('button');
-                editarButton.classList.add('bg-blue-500', 'text-white', 'px-4', 'py-2', 'rounded');
+                editarButton.classList.add('bg-blue-500', 'text-white', 'px-4', 'py-2', 'rounded','mr-3');
                 editarButton.textContent = 'Editar';
-                // editarButton.onclick = () => mostrarModalEditarProducto(producto.Codigo);
+                editarButton.onclick = () =>   mostrarModalEditarUsuario(usuario);
                 accionesCell.appendChild(editarButton);
 
-                const eliminarButton = document.createElement('button');
-                eliminarButton.classList.add('bg-red-500', 'text-white', 'px-4', 'py-2', 'rounded');
-                eliminarButton.textContent = 'Eliminar';
-                // eliminarButton.onclick = () => eliminarProducto(producto.Codigo);
-                accionesCell.appendChild(eliminarButton);
+                const estadoButton = document.createElement('button');
+                estadoButton.classList.add('bg-yellow-500', 'text-white', 'px-4', 'py-2', 'rounded','ml-3');
+                estadoButton.textContent = 'Cambiar Estado';
+                estadoButton.onclick = () => actualizarEstado(usuario.Usuario,usuario);
+                accionesCell.appendChild(estadoButton);
         
 
                 row.appendChild(accionesCell);
